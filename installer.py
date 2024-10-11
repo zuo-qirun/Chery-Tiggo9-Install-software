ef28cebecd08a2cd3a79a11e5f2a509abd121777
zuo-qirun <zuoqirun2009@126.com>
2024-10-11 21:07:30 +0800

    新增部分功能

--- installer.py
+++ installer.py
@@ -3,21 +3,12 @@
 # 此代码由zuo-qirun/B站undefined_左提供
 # V9版本 大版本更新！！新增禁用高德、打开高德头枕播放等多种功能！！
 
-import os, sys, easygui, re, argparse
+import os, sys, easygui, re, subprocess, argparse
 import modechooser
 import OutputColoredText as oct
 
-print("---------------初始化---------------")
-
-modes = ["退出程序", "安装模式", "卸载模式(需要你知道卸载软件的包名)",
-         "解决安装后高德与原车机高德HUD闪的问题(禁用原车机高德地图)", 
-         "解禁原车机高德地图(恢复原车机高德地图)", "头枕播放解决方案", 
-         "adb输入指令模式", "激活shizuku", "激活权限狗", "清除缓存"]
-ModeChooser = modechooser.Mode(modes)
-
-
 def isLegalString(string):
-    return re.match("^[a-zA-Z0-9_\-\.\(\)]*$", string)
+    return re.match("^[a-zA-Z0-9()._-]*$", string)
 
 def chooseMode():
     global ModeChooser
@@ -37,6 +28,62 @@ def chooseMode():
 
     print("************************选择模式******************************")
     return ModeChooser.getmode()
+
+
+def Install(path: str):
+    # 检测并生成新文件名
+    filename = os.path.basename(path)
+    if filename[-4:] != ".apk":
+        print(oct.ColoredText([("警告：您选择的文件后缀名不为.apk，请检查文件是否正确", "red")]))
+        print(oct.ColoredText([("已自动将文件后缀名改为apk", "normal")]))
+        filename = filename + ".apk"
+    print(f"您的文件名：{filename}")
+
+    if not isLegalString(filename):
+        print(oct.ColoredText([("检测到您的文件名不合法，文件名仅能包含", "normal"), 
+                                ("英文字符 数字 () .", "red")]))
+        filename = input(oct.ColoredText([("请输入新文件名: ", "normal")]))
+        if filename == "":
+            print(oct.ColoredText([("由于您未输入任何文件名，已将文件名改为The_apk_to_be_installed.apk", "normal")]))
+            filename = "The_apk_to_be_installed.apk"
+        elif len(filename) <= 4 or filename[-4:] != ".apk":
+            filename = filename + ".apk"
+            print(oct.ColoredText([(f"您新输入的文件名：{filename}", "normal")]))
+    else:
+        print(oct.ColoredText([("您的文件名合法", "normal")]))
+
+    if os.system(f"adb push \"{path}\" \"/data/local/tmp/{filename}\""):
+        print(oct.ColoredText([("上传失败，请截图并联系作者", "red")]))
+    
+    print("---------------正在安装---------------")
+    print(oct.ColoredText([("请", "normal"), 
+                            ("右键这条信息", "red"), 
+                            (f"，或复制:  pm install -g /data/local/tmp/{filename}, 并", "normal"), 
+                            ("点击回车", "red")]))
+    print("输入后，请手动输入exit并回车")
+    os.system(f"echo pm install -g /data/local/tmp/{filename} | clip")
+    if os.system("adb shell"):
+        print(oct.ColoredText([("启动命令行失败或命令行命令出错，请截图并联系作者", "red")]))
+    """
+    if input("请确认安装方式 1: 自动安装 2: 手动安装 (defule: 2)") != '1':
+        print(oct.ColoredText([("请", "normal"), 
+                                ("右键这条信息", "red"), 
+                                (f"，或复制:  pm install -g /data/local/tmp/{filename}, 并", "normal"), 
+                                ("点击回车", "red")]))
+        print("输入后，请手动输入exit并回车")
+        os.system(f"echo pm install -g /data/local/tmp/{filename} | clip")
+        if os.system("adb shell"):
+            print(oct.ColoredText([("启动命令行失败，请截图并联系作者", "red")]))
+    else:
+        if os.system(f"adb shell pm install -g /data/local/tmp/{filename}"):
+            print(oct.ColoredText([("自动安装失败，请尝试使用手动安装", "red")]))
+    
+    ans = input("是否清除缓存(Y/N, defult: N):")
+    if ans.upper() == 'Y':
+        if os.system("adb shell rm -rf /data/local/tmp/*"): #应该没写错吧......
+            print(oct.ColoredText([("清除失败，请截图并联系作者", "red")]))
+        print("清除成功")
+    """
     
 def Installation():
     loop = True
@@ -50,60 +97,8 @@ def Installation():
             if path == None:
                 break
         print(f"您的文件路径: {path}")
+        Install(path)
         
-        # 检测并生成新文件名
-        filename = os.path.basename(path)
-        if filename[-4:] != ".apk":
-            print(oct.ColoredText([("警告：您选择的文件后缀名不为.apk，请检查文件是否正确", "red")]))
-            print(oct.ColoredText([("已自动将文件后缀名改为apk", "normal")]))
-            filename = filename + ".apk"
-        print(f"您的文件名：{filename}")
-
-        if not isLegalString(filename):
-            print(oct.ColoredText([("检测到您的文件名不合法，文件名仅能包含", "normal"), 
-                                   ("英文字符 数字 () .", "red")]))
-            filename = input(oct.ColoredText([("请输入新文件名: ", "normal")]))
-            if filename == "":
-                print(oct.ColoredText([("由于您未输入任何文件名，已将文件名改为The_apk_to_be_installed.apk", "normal")]))
-                filename = "The_apk_to_be_installed.apk"
-            elif len(filename) <= 4 or filename[-4:] != ".apk":
-                filename = filename + ".apk"
-                print(oct.ColoredText([(f"您新输入的文件名：{filename}", "normal")]))
-        else:
-            print(oct.ColoredText([("您的文件名合法", "normal")]))
-
-        if os.system(f"adb push \"{path}\" \"/data/local/tmp/{filename}\""):
-            print(oct.ColoredText([("上传失败，请截图并联系作者", "red")]))
-        
-        print("---------------正在安装---------------")
-        print(oct.ColoredText([("请", "normal"), 
-                                ("右键这条信息", "red"), 
-                                (f"，或复制:  pm install -g /data/local/tmp/{filename}, 并", "normal"), 
-                                ("点击回车", "red")]))
-        print("输入后，请手动输入exit并回车")
-        os.system(f"echo pm install -g /data/local/tmp/{filename} | clip")
-        if os.system("adb shell"):
-            print(oct.ColoredText([("启动命令行失败或命令行命令出错，请截图并联系作者", "red")]))
-        """
-        if input("请确认安装方式 1: 自动安装 2: 手动安装 (defule: 2)") != '1':
-            print(oct.ColoredText([("请", "normal"), 
-                                   ("右键这条信息", "red"), 
-                                   (f"，或复制:  pm install -g /data/local/tmp/{filename}, 并", "normal"), 
-                                   ("点击回车", "red")]))
-            print("输入后，请手动输入exit并回车")
-            os.system(f"echo pm install -g /data/local/tmp/{filename} | clip")
-            if os.system("adb shell"):
-                print(oct.ColoredText([("启动命令行失败，请截图并联系作者", "red")]))
-        else:
-            if os.system(f"adb shell pm install -g /data/local/tmp/{filename}"):
-                print(oct.ColoredText([("自动安装失败，请尝试使用手动安装", "red")]))
-        
-        ans = input("是否清除缓存(Y/N, defult: N):")
-        if ans.upper() == 'Y':
-            if os.system("adb shell rm -rf /data/local/tmp/*"): #应该没写错吧......
-                print(oct.ColoredText([("清除失败，请截图并联系作者", "red")]))
-            print("清除成功")
-        """
         loopans = input("是否继续安装软件? (Y/N, defult: N):")
         loop = True if loopans == 'Y' or loopans == 'N' else False
 
@@ -192,29 +187,53 @@ def DelCache():
             print(oct.ColoredText([("清除失败，请截图并联系作者", "red")]))
         print("清除成功")
 
-while True:
-    mode = chooseMode()
-    if mode != ModeChooser.wrong_code:
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='A Installer for Chery-Tiggo9')
+    parser.add_argument('-v', '--version', action='version', version='9.6.1')
+    parser.add_argument('directory', nargs='?', default=None, help='an optional directory to process')
+    args = parser.parse_args()
+
+    if args.directory is not None:
+        print(f"Processing directory: {args.directory}")
+        Install(args.directory)
+        sys.exit(0)
+
+    print("---------------初始化---------------")
+
+    modes = ["退出程序", "安装模式", "卸载模式(需要你知道卸载软件的包名)",
+            "解决安装后高德与原车机高德HUD闪的问题(禁用原车机高德地图)", 
+            "解禁原车机高德地图(恢复原车机高德地图)", "头枕播放解决方案", 
+            "adb输入指令模式", "激活shizuku", "激活权限狗", "清除缓存", "检查更新"]
+    ModeChooser = modechooser.Mode(modes)
+
+    while True:
+        mode = chooseMode()
+        if mode != ModeChooser.wrong_code:
+            os.system("cls")
+            if mode == 0:
+                sys.exit(0)
+            elif mode == 1:
+                Installation()
+            elif mode == 2:
+                Uninstallation()
+            elif mode == 3:
+                DisableAutonavi()
+            elif mode == 4:
+                EnableAutonavi()
+            elif mode == 5:
+                ShowSolution()
+            elif mode == 6:
+                ADBMode()
+            elif mode == 7:
+                ActiveShizuku()
+            elif mode == 8:
+                ActivePermissiondog()
+            elif mode == 9:
+                DelCache()
+            elif mode == 10:
+                # subprocess.run(['runas', '/user:Administrator', '"pythonw ' + script_path + '"'])
+                subprocess.Popen("./update.exe")
+                sys.exit(0)
+        os.system("pause")
         os.system("cls")
-        if mode == 0:
-            sys.exit(0)
-        elif mode == 1:
-            Installation()
-        elif mode == 2:
-            Uninstallation()
-        elif mode == 3:
-            DisableAutonavi()
-        elif mode == 4:
-            EnableAutonavi()
-        elif mode == 5:
-            ShowSolution()
-        elif mode == 6:
-            ADBMode()
-        elif mode == 7:
-            ActiveShizuku()
-        elif mode == 8:
-            ActivePermissiondog()
-        elif mode == 9:
-            DelCache()
-    os.system("pause")
-    os.system("cls")
---
+++ update.py
@@ -0,0 +1,72 @@
+import requests
+import json
+import os
+import subprocess
+import tqdm
+
+def update_software():
+    # 备份当前版本
+    print("正在备份当前版本...")
+    try:
+        with open("installer.exe", "rb") as original_file:
+            original_data = original_file.read()
+        with open("installer.exe.bak", "wb") as backup_file:
+            backup_file.write(original_data)
+    except PermissionError:
+        print("没有足够的权限访问该文件。请检查文件权限或尝试以管理员身份运行。")
+    except:
+        print("备份失败")
+    # 获取升级信息
+    url = "https://api.zuoqirun.top/software/Chery-Tiggo9-Install-software/latest.json"
+    try:
+        response = requests.get(url)
+        data = json.loads(response.text)
+
+    except:
+        print("获取升级信息失败")
+
+    update_url, version = data["update_url"], data["version"]
+    version = data["version"]
+    print("最新版本：", version)
+    print("更新日志：\n", data["update_log"])
+
+    # 解析链接
+    try:
+        result = subprocess.run(["installer.exe", "-v"], capture_output=True, text=True)
+        print("当前版本：", result.stdout)
+        if version in result.stdout:
+            print("当前已是最新版本")
+        else:
+            ans = input("发现新版本，是否升级？(Y/N, defult: Y)")
+            if ans.lower() == "n":
+                print("取消升级")
+            else:
+                with open("installer.exe", "rb") as original_file:
+                    original_data = original_file.read()
+                with open("installer.exe.bak", "wb") as backup_file:
+                    backup_file.write(original_data)
+                print("开始升级")
+                print("正在下载升级包...")
+                update_result = requests.get(update_url)
+                print("下载完成，正在安装...")
+                with open("installer.exe", "wb") as f:
+                    f.write(update_result.content)
+                print("升级成功")
+    except OSError:
+        print("未找到安装程序或安装程序已损坏")
+        print("开始重新安装")
+        print("正在下载安装包...")
+        update_result = requests.get(update_url)
+        print("下载完成，正在安装...")
+        with open("installer.exe", "wb") as f:
+            f.write(update_result.content)
+        print("升级成功")
+    except:
+        print("升级失败，已为您回退到当前版本")
+        with open("installer.exe.bak", "rb") as backup_file:
+            backup_data = backup_file.read()
+        with open("installer.exe", "wb") as f:
+            f.write(backup_data)
+if __name__ == "__main__":
+    update_software()
+    os.system("pause")
